11a686675c0330156ee7bd877790002b0ca44dda
artiny <artiny16@gmail.com>
2016-12-02 17:00:29 +0100

    created - forgas gombra megcsinalni

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	public Vector3 center; 
+	Vector3 v =  Vector3.zero;
+	public GameObject auto;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		auto = GameObject.Find("r8_gt_3ds");
+		//v = v + new Vector3(0.1f, 0f,0f);
+
+		v = v + 0.01f * (center - transform.position).normalized;
+		transform.position = transform.position + v;
+		transform.LookAt (auto.transform);
+		MoveCenter ();
+	}
+
+	void MoveCenter(){
+		Vector2 rand = 5 * Random.insideUnitCircle;
+		Vector3 v3 = new Vector3 (rand.x, 0, rand.y);
+		center = center + v3;
+	}
+
+}
---
+++ Assets/rotate.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	float rotSpeed = 750;
+
+	public int zoom = 20;
+	public int normal = 60;
+	public float smooth = 5;
+
+	private bool isZoomed = false;
+
+
+	void OnMouseDrag(){
+		float rotX = Input.GetAxis ("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+		float rotY = Input.GetAxis ("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+		transform.Rotate(Vector3.up, -rotX);
+		//transform.Rotate(Vector3.right, -rotY);
+		Debug.Log("Text x " + rotX + "Text y "  + rotY);
+		//}
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButtonDown(2)) {
+			isZoomed = !isZoomed;
+		}
+		if (isZoomed) {
+			//Camera.main.fieldOfView = Mathf.Lerp (Camera.main.fieldOfView, zoom, Time.deltaTime * smooth);
+			Camera.main.fieldOfView = 50;
+		} else {
+			Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, normal, Time.deltaTime * smooth);
+		}
+	}
+}
